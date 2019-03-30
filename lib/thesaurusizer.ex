7c8722f6ba33e9d5c9fe2254908855dae82440a7
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-03-30 15:17:53 +0800

    Move logic to the ruby script for performance

--- lib/thesaurusizer.ex
+++ lib/thesaurusizer.ex
@@ -3,41 +3,11 @@ defmodule Thesaurusizer do
   Documentation for Thesaurusizer.
   """
 
-  # Top 15 words in Oxford English Corpus
-  @stop_words ~w(the be to of and a in that have I it for not on with)
-
   def thesaurusize(passage, delimiter \\ " ")
       when is_binary(passage) and is_binary(delimiter) and byte_size(delimiter) == 1 do
-    passage
-    |> String.split(delimiter)
-    |> Enum.map(fn word ->
-      sanitised_word = String.replace(word, ~r/[\p{P}\p{S}]/, "")
-
-      if sanitised_word in @stop_words do
-        word
-      else
-        synonyms = get_synonyms(sanitised_word)
-
-        synonyms
-        |> Enum.filter(&(not String.contains?(&1, " ")))
-        |> case do
-          [] ->
-            word
-
-          one_word_synonyms ->
-            one_word_synonyms
-            |> Enum.random()
-            |> (&String.replace(word, sanitised_word, &1)).()
-        end
-      end
-    end)
-    |> Enum.join(delimiter)
-  end
-
-  defp get_synonyms(word) do
-    case System.cmd("ruby", ["priv/synonyms.rb", word]) do
+    case System.cmd("ruby", ["priv/synonyms.rb", passage, delimiter]) do
       {output, 0} ->
-        Jason.decode!(output)
+        output
 
       _ ->
         raise "Something goes wrong with the ruby script. Check ruby and the rwordnet gem is installed"
