f0f0d791868fb5fa20b95274865fb8bb422a158d
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-10-09 20:50:57 +0800

    BUmp deps

--- mix.exs
+++ mix.exs
@@ -25,6 +25,7 @@ defmodule Thesaurusizer.MixProject do
       {:ex_gram, "~> 0.8"},
       {:exwordnet, "~> 0.1"},
       {:tesla, "~> 1.2"},
+      {:hackney, "~> 1.15"},
       {:dialyxir, "~> 1.0.0-rc.4", only: :dev, runtime: false},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
