f87d1af9ca04a84e0e25aa844224714868536e35
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2020-11-27 15:15:48 +0800

    Fix typo

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-config :thesaurusizer, name: :thesaurusizer_bot
+config :thesaurusizer, bot_name: :thesaurusizer_bot
 
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
